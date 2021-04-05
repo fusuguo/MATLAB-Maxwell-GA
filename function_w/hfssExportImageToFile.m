1c3cde8315ce2d6b04857ce7cca436e7d08b85e5
WXY <fusuruguo@gmail.com>
2021-04-06 01:14:48 +0800

    delete

--- function_w/hfssExportImageToFile.m
+++
@@ -1,4 +0,0 @@
-function hfssExportImageToFile(fid,reportName,filepath)
-fprintf(fid,'Set oModule = oDesign.GetModule("ReportSetup") \n');
-fprintf(fid,'oModule.ExportImageToFile "%s",  _\n',reportName);
-fprintf(fid,'"%s", 0, 0 \n',filepath);
